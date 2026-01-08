91f96544f671ea796815e5ee2aad8999a3dfa025
PablineSantos <pablines2006@gmail.com>
2026-01-08 14:10:28 -0300

    User entity and resource

--- src/main/java/com/maxvendas/MaxVendas/MaxVendasApplication.java
+++ src/main/java/com/maxvendas/MaxVendas/MaxVendasApplication.java
@@ -1,4 +1,4 @@
-package com.maxvendas.MaxVendas;
+package com.maxvendas.maxvendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
