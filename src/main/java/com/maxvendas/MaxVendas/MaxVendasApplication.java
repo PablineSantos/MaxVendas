d8f0a230a654960e7eb3949534a6421a62c2e6ea
PablineSantos <pablines2006@gmail.com>
2026-01-08 13:46:43 -0300

    Project created

---
+++ src/main/java/com/maxvendas/MaxVendas/MaxVendasApplication.java
@@ -0,0 +1,13 @@
+package com.maxvendas.MaxVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxvendas/MaxVendas/MaxVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxvendas.MaxVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
