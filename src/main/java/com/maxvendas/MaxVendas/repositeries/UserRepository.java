ce2ca5bd550d24a9ac60827c7bc52b15585439fb
PablineSantos <pablines2006@gmail.com>
2026-01-09 10:38:16 -0300

    Service layer, component registration

--- src/main/java/com/maxvendas/MaxVendas/repositeries/UserRepository.java
+++ src/main/java/com/maxvendas/MaxVendas/repositeries/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.maxvendas.maxvendas.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> // (Entidade que ele gerencia, Tipo do ID)
+{
+
 	
 }
